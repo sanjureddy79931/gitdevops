85c50f27ffb8758b2f6b104bb6e3f079e5485d94
sanjureddy79931 <sanjureddy79931@gmail.com>
2026-07-21 11:38:09 +0530

    Integrate Prometheus metrics for multiplication
    Added Prometheus metrics for multiplication results and pushed to Pushgateway.

--- mul.py
+++ mul.py
@@ -1,8 +1,26 @@
 import csv
-with open("numbers.csv",mode="r") as f:
-	reader=csv.DictReader(f)
-	for row in reader:
-		num1=float(row["num1"])
-		num2=float(row["num2"])
-		print(f"Multiplication  Result -> {num1} x {num2}={num1*num2}")
-		
+from prometheus_client import Gauge, push_to_gateway, REGISTRY
+
+# Define the metric for multiplication
+g = Gauge("multiplication", "Sum of multiplications")
+total = 0.0
+
+with open("numbers.csv", mode="r") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        num1 = float(row["num1"])
+        num2 = float(row["num2"])
+        
+        # Calculate the multiplication
+        result = num1 * num2
+        print(f"Multiplication Result -> {num1} * {num2} = {result}")
+        
+        # Add to the total
+        total += result 
+
+# Set the final calculated total to the Gauge
+g.set(total)
+
+# Push to the gateway with a unique job name
+push_to_gateway("192.168.49.1:9091", job="multiplication_job", registry=REGISTRY)	
+print("Multiplication metrics successfully pushed to Prometheus Pushgateway.")
