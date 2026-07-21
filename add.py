3563f006337b2d03e405e206df97614ba32f834d
sanjureddy79931 <sanjureddy79931@gmail.com>
2026-07-21 10:44:16 +0530

    Integrate Prometheus metrics and change operation
    Added Prometheus metrics for addition results and changed operation to subtraction.

--- add.py
+++ add.py
@@ -1,8 +1,16 @@
 import csv
+from prometheus_client import Gauge,push_to_gateway, REGISTRY
+
+g=Gauge("addition","Sum of additions")
+total=0.0
 with open("numbers.csv",mode="r") as f:
 	reader=csv.DictReader(f)
 	for row in reader:
 		num1=float(row["num1"])
 		num2=float(row["num2"])
-		print(f"Addition Result -> {num1}+{num2}={num1+num2}")
+		print(f"Addition Result -> {num1}-{num2}={num1-num2}")
+
+g.set(total)
+push_to_gateway("192.168.49.1:9091",job="add_job",registry=REGISTRY)	
+print("Prometheus")
 		
